6b8890ed0f5e209d4469406c55325e1f633fce88
Sonu Yadav <sonukyadav2510@gmail.com>
2021-07-12 00:40:10 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
       $u = mysqli_real_escape_string($conn, $key);
       $new_url = str_replace('/', '', $u);
     }
+	echo $u;
       $sql = mysqli_query($conn, "SELECT full_url FROM url WHERE shorten_url = '{$new_url}'");
       if(mysqli_num_rows($sql) > 0){
         $sql2 = mysqli_query($conn, "UPDATE url SET clicks = clicks + 1 WHERE shorten_url = '{$new_url}'");
